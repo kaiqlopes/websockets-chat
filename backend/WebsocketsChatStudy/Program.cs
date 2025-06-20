27c74eb257e415e957d30d6ae6991c1da0144c33
Kaique Lopes <kaique.lopes@copart.com>
2025-06-20 13:30:44 -0300

    fix: update Identity configuration in Program.cs

--- backend/WebsocketsChatStudy/Program.cs
+++ backend/WebsocketsChatStudy/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddIdentity<User, IdentityRole>()
+builder.Services.AddIdentity<User, Role>()
     .AddEntityFrameworkStores<ChatContext>()
     .AddDefaultTokenProviders();
 
